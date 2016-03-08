3be1a0fb607d279df71c3959ef153491e4129802
Christophe <christophe.meyers@outlook.com>
2016-03-08 10:08:27 +0000

    Chris - Setting up Mailing

--- be_friend_a_child/createlogin.php
+++ be_friend_a_child/createlogin.php
@@ -7,6 +7,7 @@ I'll <?php
 	//if the method called is a "POST"
 	else if ($_SERVER['REQUEST_METHOD']==='POST'){
 		add_to_database();//call the function "add_to_database"
+		email_volunteer_login();//call the function "email_volunteer_login()"
 	}
 	
 	function add_to_database(){
@@ -100,6 +101,60 @@ I'll <?php
 		else{
 			header("Location: index.php");
 		}
+	}
+
+
+	//email to volunteer function
+	function email_volunteer_login(){
+
+		//setting some variables with form values
+		$firstname = $_POST["firstname"];
+		$surname = $_POST["surname"];
+		$password = $_POST["password"];
+		$email = $_POST["email"];
+		$name = $firstname . " " . $surname;
+
+		//email subject
+		$subject = "Befriend A Child - Survey Login";
+
+		//email body in html
+		$txt = "Dear $name,
+                <br><br>
+                An account has been set up in your name.
+                <br>
+                If you would like to fill out a survey concerning your experience with Befriend A Child,
+                please follow
+                <a href='http://befriendachildtestsurvey.azurewebsites.net/volunteerlogin.html'>this link</a>
+                and login with:
+                <br><br>
+                Username: $email
+                <br>
+                Password: $password
+                <br><br>
+                You will be able to change your password once logged in.
+                <br><br>
+                King Regards,
+                <br><br>
+                The Befriend A Child Team";
+
+
+
+		require_once 'swiftmailer/lib/swift_required.php';
+
+		$transporter = Swift_SmtpTransport::newInstance('smtp.gmail.com', 465, "ssl")
+			->setUsername('christophe.meyers.312@gmail.com')
+			->setPassword('AnnachengAddress');
+
+		$mailer = Swift_Mailer::newInstance($transporter);
+
+		$message = Swift_Message::newInstance('Befriend A Child Test Mail')
+			->setFrom(array('christophe.meyers.312@gmail.com' => 'Christophe Meyers'))
+			->setTo(array($email => $name))
+			->setBody($txt, "text/html");
+
+		$result = $mailer->send($message);
+
+
 	}
 	
 		function show_create_user() {

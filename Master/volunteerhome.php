18eb1a59cc171c3fbed5819539ab72d2f61c52bd
kannandineshan <d.kannan@rgu.ac.uk>
2016-03-19 22:23:05 +0000

    19/03/16 Dinesh pushing Volunteer home page +CSS and JQUERY

--- Master/volunteerhome.php
+++ Master/volunteerhome.php
@@ -15,6 +15,11 @@
 
     <!-- CSS Stylesheet-->
     <link rel="stylesheet" href="volunteerhome_assets/volunteerhome_css/volunteerhome.css" type="text/css">
+
+    <!-- JQUERY SCRIPT-->
+    <link rel="stylesheet" href="//code.jquery.com/ui/1.11.4/themes/smoothness/jquery-ui.css">
+    <script src="//code.jquery.com/jquery-1.10.2.js"></script>
+    <script src="//code.jquery.com/ui/1.11.4/jquery-ui.js"></script>
 </head>
 <!-- [END OF HEAD] ----------------------------------------------------------------------------------------------->
 
@@ -22,38 +27,44 @@
 <!-- [START OF BODY] --------------------------------------------------------------------------------------------->
 <body>
 
-<!-- [START OF SURVEY] ------------------------------------------------------------------->
-<form>
+    <!-- (START OF MAIN) ------------------------------------------------------------------->
+    <main class="grid-container">
 
-<!-- SURVEY QUESTION 1 -->
+    <!-- SURVEY SUBMIT BUTTON -->
+    <section class="container" id="cont7">
+        <input type="submit" value="SUBMIT SURVEY">
+    </section>
 
-        <label for="question1">What activities did you do today ? :</label>
-        <br>
-        <textarea name="question1" cols="45" rows="5" placeholder="Enter your response here"></textarea>
-    <br>
-
-<!-- SURVEY QUESTION 2 -->
-
-        <label for="question2">How much did you spend today ? :</label>
-        <br>
-        <input type="number" step="any" name="question2" placeholder="Enter Amount">
-    <br>
-
-<!-- SURVEY QUESTION 3 -->
-
-        <label for="question3">How much fun did you have today ? :</label>
-        <input type="radio" name="question3"><img src="volunteerhome_assets/volunteerhome_images/surveyiconsad.png">
-        <input type="radio" name="question3"><img src="volunteerhome_assets/volunteerhome_images/surveyiconnomal.png">
-        <input type="radio" name="question3"><img src="volunteerhome_assets/volunteerhome_images/surveyiconsmile.png">
+    <!-- SURVEY QUESTION 6 -->
+    <section class="container" id="cont6">
+        <label for="question6">Would you want to do it again ? :</label>
+        <input type="radio" name="question6">YES
+        <input type="radio" name="question6">NO
         <br>
         <br>
-        <label for="question3">Explain :</label>
+        <label for="question6">Explain :</label>
         <br>
-        <textarea name="question3" cols="45" rows="5" placeholder="Enter your explanation here"></textarea>
-    <br>
+        <textarea name="question6" cols="45" rows="5" placeholder="Explain why here"></textarea>
+        <br>
+        <button id="button6">CLICK HERE FOR NEXT QUESTION </button>
+    </section>
 
-<!-- SURVEY QUESTION 4 -->
+    <!-- SURVEY QUESTION 5 -->
+    <section class="container" id="cont5">
+        <label for="question5">Did you eat something healthy ? :</label>
+        <input type="radio" name="question5">YES
+        <input type="radio" name="question5">NO
+        <br>
+        <br>
+        <label for="question5">Explain :</label>
+        <br>
+        <textarea name="question5" cols="45" rows="5" placeholder="Enter items here"></textarea>
+        <br>
+        <button id="button5">CLICK HERE FOR NEXT QUESTION </button>
+    </section>
 
+    <!-- SURVEY QUESTION 4 -->
+    <section class="container" id="cont4">
         <label for="question4">Did you learn something new ? :</label>
         <input type="radio" name="question4">Nothing new
         <input type="radio" name="question4">Done it before
@@ -63,37 +74,51 @@
         <label for="question4">Explain :</label>
         <br>
         <textarea name="question4" cols="45" rows="5" placeholder="Explain what you learned here"></textarea>
-    <br>
+        <br>
+        <button id="button4">CLICK HERE FOR NEXT QUESTION </button>
+    </section>
 
-<!-- SURVEY QUESTION 5 -->
-
-        <label for="question5">Did you eat something healthy ? :</label>
-        <input type="radio" name="question5">YES
-        <input type="radio" name="question5">NO
+    <!-- SURVEY QUESTION 3 -->
+    <section class="container" id="cont3">
+        <label for="question3">How much fun did you have today ? :</label>
+        <input type="radio" name="question3"><img src="volunteerhome_assets/volunteerhome_images/surveyiconsad.png">
+        <input type="radio" name="question3"><img src="volunteerhome_assets/volunteerhome_images/surveyiconnomal.png">
+        <input type="radio" name="question3"><img src="volunteerhome_assets/volunteerhome_images/surveyiconsmile.png">
         <br>
         <br>
-        <label for="question5">Explain :</label>
+        <label for="question3">Explain :</label>
         <br>
-        <textarea name="question5" cols="45" rows="5" placeholder="Enter items here"></textarea>
-    <br>
-
-<!-- SURVEY QUESTION 6 -->
-
-        <label for="question6">Would you want to do it again ? :</label>
-        <input type="radio" name="question6">YES
-        <input type="radio" name="question6">NO
+        <textarea name="question3" cols="45" rows="5" placeholder="Enter your explanation here"></textarea>
         <br>
-        <br>
-        <label for="question6">Explain :</label>
-        <br>
-        <textarea name="question6" cols="45" rows="5" placeholder="Explain why here"></textarea>
-    <br>
-    <br>
+        <button id="button3">CLICK HERE FOR NEXT QUESTION </button>
+    </section>
 
-    <input type="submit" value="SUBMIT SURVEY">
+    <!-- SURVEY QUESTION 2 -->
+    <section class="container" id="cont2">
+        <label for="question2">How much did you spend today ? :</label>
+        <br>
+        <input type="number" step="any" name="question2" placeholder="Enter Amount">
+        <br>
+        <button id="button2">CLICK HERE FOR NEXT QUESTION </button>
+    </section>
 
-</form>
-<!-- [END OF SURVEY] ------------------------------------------------------------------->
+    <!-- SURVEY QUESTION 1 -->
+    <section class="container" id="cont1">
+        <label for="question1">What activities did you do today ? :</label>
+        <br>
+        <textarea name="question1" cols="45" rows="5" placeholder="Enter your response here"></textarea>
+        <br>
+        <button id="button1">CLICK HERE FOR NEXT QUESTION </button>
+    </section>
+
+
+    <!-- CALL JQUERY SCRIPT FUNCTION-->
+    <script SRC="volunteerhome_assets/volunteerhome_jquery/surveybounce.js"></script>
+
+
+    </main>
+    <!-- (END OF MAIN) ------------------------------------------------------------------->
+
 </body>
 <!-- [END OF BODY] --------------------------------------------------------------------------------------------->
 

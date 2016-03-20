07f837d3cbbb3259aed12169bff9c4c6ebfed577
kannandineshan <d.kannan@rgu.ac.uk>
2016-03-20 00:26:09 +0000

    2003/16 Dinesh volunteer homepage CSS + JQUERY

--- Master/volunteerhome.php
+++ Master/volunteerhome.php
@@ -37,78 +37,32 @@
 
     <!-- SURVEY QUESTION 6 -->
     <section class="container" id="cont6">
-        <label for="question6">Would you want to do it again ? :</label>
-        <input type="radio" name="question6">YES
-        <input type="radio" name="question6">NO
-        <br>
-        <br>
-        <label for="question6">Explain :</label>
-        <br>
-        <textarea name="question6" cols="45" rows="5" placeholder="Explain why here"></textarea>
-        <br>
-        <button id="button6">CLICK HERE FOR NEXT QUESTION </button>
+        <?php include ("volunteerhome_assets/volunteerhome_htmlscripts/question6.html"); ?>
     </section>
 
     <!-- SURVEY QUESTION 5 -->
     <section class="container" id="cont5">
-        <label for="question5">Did you eat something healthy ? :</label>
-        <input type="radio" name="question5">YES
-        <input type="radio" name="question5">NO
-        <br>
-        <br>
-        <label for="question5">Explain :</label>
-        <br>
-        <textarea name="question5" cols="45" rows="5" placeholder="Enter items here"></textarea>
-        <br>
-        <button id="button5">CLICK HERE FOR NEXT QUESTION </button>
+        <?php include ("volunteerhome_assets/volunteerhome_htmlscripts/question5.html"); ?>
     </section>
 
     <!-- SURVEY QUESTION 4 -->
     <section class="container" id="cont4">
-        <label for="question4">Did you learn something new ? :</label>
-        <input type="radio" name="question4">Nothing new
-        <input type="radio" name="question4">Done it before
-        <input type="radio" name="question4">Never done it before
-        <br>
-        <br>
-        <label for="question4">Explain :</label>
-        <br>
-        <textarea name="question4" cols="45" rows="5" placeholder="Explain what you learned here"></textarea>
-        <br>
-        <button id="button4">CLICK HERE FOR NEXT QUESTION </button>
+        <?php include ("volunteerhome_assets/volunteerhome_htmlscripts/question4.html"); ?>
     </section>
 
     <!-- SURVEY QUESTION 3 -->
     <section class="container" id="cont3">
-        <label for="question3">How much fun did you have today ? :</label>
-        <input type="radio" name="question3"><img src="volunteerhome_assets/volunteerhome_images/surveyiconsad.png">
-        <input type="radio" name="question3"><img src="volunteerhome_assets/volunteerhome_images/surveyiconnomal.png">
-        <input type="radio" name="question3"><img src="volunteerhome_assets/volunteerhome_images/surveyiconsmile.png">
-        <br>
-        <br>
-        <label for="question3">Explain :</label>
-        <br>
-        <textarea name="question3" cols="45" rows="5" placeholder="Enter your explanation here"></textarea>
-        <br>
-        <button id="button3">CLICK HERE FOR NEXT QUESTION </button>
+        <?php include ("volunteerhome_assets/volunteerhome_htmlscripts/question3.html"); ?>
     </section>
 
     <!-- SURVEY QUESTION 2 -->
     <section class="container" id="cont2">
-        <label for="question2">How much did you spend today ? :</label>
-        <br>
-        <input type="number" step="any" name="question2" placeholder="Enter Amount">
-        <br>
-        <button id="button2">CLICK HERE FOR NEXT QUESTION </button>
+        <?php include ("volunteerhome_assets/volunteerhome_htmlscripts/question2.html"); ?>
     </section>
 
     <!-- SURVEY QUESTION 1 -->
     <section class="container" id="cont1">
-        <label for="question1">What activities did you do today ? :</label>
-        <br>
-        <textarea name="question1" cols="45" rows="5" placeholder="Enter your response here"></textarea>
-        <br>
-        <button id="button1">CLICK HERE FOR NEXT QUESTION </button>
+        <?php include ("volunteerhome_assets/volunteerhome_htmlscripts/question1.html"); ?>
     </section>
 
 

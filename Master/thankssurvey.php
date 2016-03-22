d0be71739861237fc971987ca05098071e5b5faf
Christophe <christophe.meyers@outlook.com>
2016-03-22 15:37:12 +0000

    22/03 Chris - adding own changes to Master

---
+++ Master/thankssurvey.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Christophe
+ * Date: 22/03/2016
+ * Time: 15:32
+ */
+
+
+session_start();
+if(!isset($_SESSION['vol_email'])){
+    header("Location: volunteerlogin.php");
+}
+
+
+echo "<script>alert('Thank you for completing the survey!');</script>";
+
+
+
+
+?>
+
+
+<!DOCTYPE html>
+    <html>
+<head>
+    <meta charset="utf-8">
+    <title>Thank you for submitting the survey!</title>
+</head>
+<body>
+    <a href="volunteerhub.php"></a>
+</body>
+</html>

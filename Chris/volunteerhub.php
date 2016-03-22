a316b41f1d6a6f37f246338dc84f575f0f855a2f
Christophe <christophe.meyers@outlook.com>
2016-03-22 10:26:28 +0000

    22/03 Chris - Making new database tables work

--- Chris/volunteerhub.php
+++ Chris/volunteerhub.php
@@ -67,12 +67,7 @@
 
     function get_volunteer_name($email){
 
-        $db = new MySQLi(
-            'ap-cdbr-azure-east-c.cloudapp.net', //server or host address
-            'b35e94884f471c', //username for connecting to database
-            '90efdea3', //user's password
-            'befriendachildtestDB' //database being connected to
-        );
+        include("db_connection.php");
 
         if($db->connect_errno){
             die('Connectfailed['.$db->connect_error.']');

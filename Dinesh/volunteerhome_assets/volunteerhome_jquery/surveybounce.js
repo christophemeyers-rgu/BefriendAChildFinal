9797dcf7af946bf1f9fb685ff280a2994e472585
kannandineshan <d.kannan@rgu.ac.uk>
2016-03-22 01:51:39 +0000

    21/03/16 Dinesh updating code on volunteer page

--- Dinesh/volunteerhome_assets/volunteerhome_jquery/surveybounce.js
+++ Dinesh/volunteerhome_assets/volunteerhome_jquery/surveybounce.js
@@ -1,25 +1,25 @@
 
-$( "#submit" ).click(function() {
+$( "#submit1" ).click(function() {
     $( "#cont1" ).toggle( "bounce", 1000 );
 });
 
-$( "#submit" ).click(function() {
+$( "#submit2" ).click(function() {
     $( "#cont2" ).toggle( "size", 1000 );
 });
 
-$( "#submit" ).click(function() {
+$( "#submit3" ).click(function() {
     $( "#cont3" ).toggle( "fold", 1000 );
 });
 
-$( "#submit" ).click(function() {
+$( "#submit4" ).click(function() {
     $( "#cont4" ).toggle( "puff", 1000 );
 });
 
-$( "#submit" ).click(function() {
+$( "#submit5" ).click(function() {
     $( "#cont5" ).toggle( "clip", 1000 );
 });
 
-$( "#submit" ).click(function() {
+$( "#submit6" ).click(function() {
     $( "#cont6" ).toggle( "scale", 1000 );
 });
 

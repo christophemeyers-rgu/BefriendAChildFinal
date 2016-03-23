33d975a532597304bab752353cc5050ddc2fc76a
Christophe <christophe.meyers@outlook.com>
2016-03-23 23:23:12 +0000

    23/03 - Chris cleaning up code

---
+++ Chris/volunteerhome_assets/volunteerhome_htmlscripts/question6.php
@@ -0,0 +1,17 @@
+<label for="question6">
+    <?php
+        //function for getting the text
+        get_question_text(51);
+
+    ?>
+</label>
+<input hidden type="number" name="qid6" value="51"><!-- Hidden input that carries value of question id (currently manual)-->
+<input required type="radio" name="question6" value=true>YES
+<input required type="radio" name="question6" value=false>NO
+<br>
+<br>
+<label for="question6">Explain :</label>
+<br>
+<textarea name="question6_opt" cols="45" rows="5" placeholder="Explain why here"></textarea>
+<br>
+<input type="button" id="submit6" name="submit" value="GO TO SUBMIT SURVEY">

dfdebba511f22a063eed68eb13b0d01cf51cb0a9
Christophe <christophe.meyers@outlook.com>
2016-03-23 20:53:27 +0000

    23/03 - Chris cleaning up code

--- Chris/logoutvolunteer.php
+++ Chris/logoutvolunteer.php
@@ -6,9 +6,12 @@
  * Time: 09:34
  */
 
+
+    //THIS PAGE LOGS OUT A VOLUNTEER
+
 	session_start();
 	if(session_destroy()) // Destroying All Sessions
     {
-        header("Location: volunteerlogin.php"); // Redirecting To Home Page
+        header("Location: volunteerlogin.php"); // Redirecting To Login Page
     }
 ?>

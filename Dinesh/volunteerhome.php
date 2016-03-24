c2420972f47a2d0bce8fad6a80935c2fac531be8
kannandineshan <d.kannan@rgu.ac.uk>
2016-03-24 00:46:42 +0000

    23/03/16   Dinesh  Survey Summary

--- Dinesh/volunteerhome.php
+++ Dinesh/volunteerhome.php
@@ -81,7 +81,7 @@
 
     <!- - SURVEY SUBMIT BUTTON - ->
     <section class="container" id="cont7">
-       <?php include("volunteerhome_assets/volunteerhome_htmlscripts/question6.php"); ?>
+        <?php include("volunteerhome_assets/volunteerhome_htmlscripts/question1.php"); ?>
         <input type="submit" id="submit" name="submit" value="SUBMIT SURVEY">
     </section>
 

114e1fac79e87727b6ae749ed9f017013dbb18d8
kannandineshan <d.kannan@rgu.ac.uk>
2016-03-24 04:15:50 +0000

    23/03/16   Dinesh  Survey Summary

--- Dinesh/volunteerhome_assets/volunteerhome_htmlscripts/question1.php
+++ Dinesh/volunteerhome_assets/volunteerhome_htmlscripts/question1.php
@@ -4,7 +4,7 @@
     ?>
 </label>
 <br>
-<input hidden type="number" name="qid1" value="1">
+
 <textarea  name="question1" cols="45" rows="5" placeholder="Enter your response here" required></textarea>
 <br>
 <input type="button" id="submit1" name="submit" value="GO TO QUESTION 2">

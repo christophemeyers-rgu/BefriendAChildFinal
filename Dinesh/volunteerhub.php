f596b206c63da3a8bc9140d15f4db9e16a642903
kannandineshan <d.kannan@rgu.ac.uk>
2016-03-24 06:59:54 +0000

    24/03/16   Dinesh  Survey Summary

--- Dinesh/volunteerhub.php
+++ Dinesh/volunteerhub.php
@@ -106,6 +106,11 @@
 
     <!- - CSS Stylesheet- ->
     <link rel="stylesheet" href="volunteerhub_css/volunteerhub.css" type="text/css">
+
+    <!- - JQUERY SCRIPT- ->
+    <link rel="stylesheet" href="//code.jquery.com/ui/1.11.4/themes/smoothness/jquery-ui.css">
+    <script src="//code.jquery.com/jquery-1.10.2.js"></script>
+    <script src="//code.jquery.com/ui/1.11.4/jquery-ui.js"></script>
 </head>
 <!- - [END OF HEAD] - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - ->
 

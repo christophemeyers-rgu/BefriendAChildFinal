c8990a99baf0107ecbfb85e9a7931e796e5b0e37
kannandineshan <d.kannan@rgu.ac.uk>
2016-03-25 04:20:20 +0000

    25/03/16 Dinesh Survey Summary

--- Dinesh/volunteerhome_assets/volunteerhome_jquery/surveybounce.js
+++ Dinesh/volunteerhome_assets/volunteerhome_jquery/surveybounce.js
@@ -25,7 +25,7 @@ $( "#submit6" ).click(function() {
 
         //$("#summary").html($("Form").serialize());
 
-    $( "input:not([type=button])" ).clone().prependTo( "p" );
+    $( "input[type=text], input[type=number], input[type=radio]" ).clone().prependTo( "p" );
 
 });
 

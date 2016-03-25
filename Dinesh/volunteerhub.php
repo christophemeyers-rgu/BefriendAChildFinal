96ca938273aaa15dca403fbcdec3a1a72270de0e
kannandineshan <d.kannan@rgu.ac.uk>
2016-03-25 06:26:10 +0000

    25/03/16 Dinesh Survey Summary

--- Dinesh/volunteerhub.php
+++ Dinesh/volunteerhub.php
@@ -131,9 +131,12 @@
 
         <section class="container" id="cont1">
             <a href="volunteerhome.php"> <input id="startsurvey" type="button" value="Start survey"></a><br><br>
-
-    <a href="logoutvolunteer.php" id="logout"><input id="logout" type="button" value="Logout"></a>
         </section>
+
+        <section class="container" id="cont2">
+    <a href="logoutvolunteer.php" id="logout"><input id="logout" type="button" value="Logout"></a>
+        <section class="container" id="cont2">
+
 
     </main>
 <!- - (END OF MAIN) - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - ->

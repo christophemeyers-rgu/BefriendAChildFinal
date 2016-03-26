afe9d254ab2926cde76f785fcf5659112d678b36
kannandineshan <d.kannan@rgu.ac.uk>
2016-03-26 08:38:05 +0000

    26/03/16 Dinesh Survey Summary

--- Dinesh/volunteerhome_assets/volunteerhome_jquery/surveybounce.js
+++ Dinesh/volunteerhome_assets/volunteerhome_jquery/surveybounce.js
@@ -1,26 +1,26 @@
 
 $( "#submit1" ).click(function() {
-    $( "#cont1" ).show( "fade", 1000 );
+    $( "#cont2" ).show( "fade", 1000 );
 });
 
 $( "#submit2" ).click(function() {
-    $( "#cont2" ).show( "size", 1000 );
+    $( "#cont3" ).show( "size", 1000 );
 });
 
 $( "#submit3" ).click(function() {
-    $( "#cont3" ).show( "fold", 1000 );
+    $( "#cont4" ).show( "fold", 1000 );
 });
 
 $( "#submit4" ).click(function() {
-    $( "#cont4" ).show( "puff", 1000 );
+    $( "#cont5" ).show( "puff", 1000 );
 });
 
 $( "#submit5" ).click(function() {
-    $( "#cont5" ).show( "clip", 1000 );
+    $( "#cont6" ).show( "clip", 1000 );
 });
 
 $( "#submit6" ).click(function() {
-    $( "#cont6" ).show( "scale", 1000 );
+    $( "#cont7" ).show( "scale", 1000 );
 
 
         //$("#summary").html($("Form").serialize());
@@ -31,5 +31,5 @@ $( "#submit6" ).click(function() {
 });
 
 $( "#submit7" ).click(function() {
-    $( "#cont7" ).toggle( "slide", 1000 );
+    $( "#cont8" ).show( "slide", 1000 );
 });

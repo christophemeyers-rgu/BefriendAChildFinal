9ca18a4135284c72bff7d5674f20d106ea59dfc3
chudiezeks <chukwudiezekwesili@windowslive.com>
2016-03-27 06:18:03 +0100

    view

--- Chukwudi/View.php
+++ Chukwudi/View.php
@@ -53,7 +53,7 @@
                             <tbody>
                               <tr>
                                 <td><?php echo $counter; ?></td>
-                                <td><a href=""><?php echo $row['vol_email']; ?></a></td>
+                                <td><?php echo $row['vol_email']; ?></td>
                                 <td><?php echo $row['vol_firstname']; ?></td>
                                 <td><?php echo $row['vol_surname']; ?></td>
                               </tr>

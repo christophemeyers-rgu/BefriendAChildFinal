8bed5ea5d5e5e44d195ca39aa2dd20e1ff3f0c2c
chudiezeks <chukwudiezekwesili@windowslive.com>
2016-03-27 04:46:57 +0100

    more

--- Chukwudi/View.php
+++ Chukwudi/View.php
@@ -53,7 +53,7 @@
 								<tbody>
                                   <tr>
                                     <td><?php echo $counter; ?></td>
-                                    <td><a href="edit-user.php?vol_email=<?php echo $row['vol_email']; ?>" style="color:green;"><?php echo $row['vol_email']; ?></a></td>
+                                    <td><a href="edit-user.php?vol_email=<?php echo $row['vol_email']; ?>"><?php echo $row['vol_email']; ?></a></td>
                                     <td><?php echo $row['vol_firstname']; ?></td>
                                     <td><?php echo $row['vol_surname']; ?></td>
                                   </tr>

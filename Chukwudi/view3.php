85733be51a5e20a360dbcb84dfd99475461edaf1
chudiezeks <chukwudiezekwesili@windowslive.com>
2016-03-29 09:10:42 +0100

    updates

--- Chukwudi/view3.php
+++ Chukwudi/view3.php
@@ -5,4 +5,13 @@
  * Date: 29/03/2016
  * Time: 09:05
  */
+
+    include 'functions.php';
+
+    session_start();
+    if(!isset($_SESSION['vol_email'])){
+        header("Location: volunteerlogin.php");
+    }
+
+
 ?>

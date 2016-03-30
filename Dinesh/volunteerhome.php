de9c054d9871bacb34cd6dced3b915707ed3ad9a
kannandineshan <d.kannan@rgu.ac.uk>
2016-03-30 15:49:47 +0100

    Dinesh 29/03/16 Volunteer page styling

--- Dinesh/volunteerhome.php
+++ Dinesh/volunteerhome.php
@@ -129,7 +129,7 @@ function get_volunteer_name($email){
 
         <!-- Logout Button -->
         <section class="header" id="logout">
-            <h2> <a href="logoutvolunteer.php" id="logout"><input type="button" value="Logout"></a></h2>
+            <h5> <a href="logoutvolunteer.php" id="logout"><input type="button" value="Logout"></a></h5>
         </section>
 
     </header>

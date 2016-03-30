efeb7ad7f70728d9b82c747357d19e6f88a0d81b
kannandineshan <d.kannan@rgu.ac.uk>
2016-03-30 16:23:09 +0100

    Dinesh 29/03/16 Volunteer page styling

--- Dinesh/volunteerhome.php
+++ Dinesh/volunteerhome.php
@@ -141,6 +141,7 @@ function get_volunteer_name($email){
     <!-- (START OF MAIN) - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -->
     <main>
 
+
         <!-- (START OF SURVEY) ---------------------------------------------------------------------- -->
         <form class="main" id="surveyform" action="submitsurveyanswers.php" method="post" >
 
@@ -197,41 +198,14 @@ function get_volunteer_name($email){
 
             <!-- (START OF WELCOME PAGE) ---------------------------------------------------------------------- -->
             <section class="form" id="welcomepage">
-
-                <!-- Volunteer name is printed in the head -->
-                <!--function from above using the email pulled from the session-->
-                <h1>Welcome<br><?php get_volunteer_name($_SESSION['vol_email']);?> </h1>
-
-                <!-- Survey Instructions -->
-                <h4>
-                    -There are 6 questions in total.
-                    <br><br>
-                    -A summary page at the end allows you to check your answers and change them if needed.
-                    <br><br>
-                   - All questions indicated as "required" must be answered to be able to submit the survey.
-                    <br><br>
-                    Thank you.
-                </h4>
-
-                <div>
-                <h2>======[To begin survey enter the date of outing]:======</h2>
-                <fieldset>
-                <label for="eventdate">Enter date (mm/dd/yy)</label>
-                <input required type="date" name="eventdate">
-                </fieldset>
-                </div>
-                <br>
-
-                <!-- Start Survey Button -->
-                <input type="button" id="start" name="next" value="Start Survey">
-
+                <?php include ("volunteerhome_assets/volunteerhome_htmlscripts/welcomepage.php")
+                ?>
             </section>
             <!-- (END OF WELCOME PAGE) ---------------------------------------------------------------------- -->
 
 
         </form>
         <!-- (END OF SURVEY) ---------------------------------------------------------------------- -->
-
 
 
     </main>

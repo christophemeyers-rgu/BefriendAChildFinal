f10891a208083f04da368a62aa9af1acab541db9
chudiezeks <chukwudiezekwesili@windowslive.com>
2016-04-09 08:01:40 +0100

    test

--- Chukwudi/submitsurveyanswers.php
+++ Chukwudi/submitsurveyanswers.php
@@ -56,7 +56,7 @@
         $date="date'".date("Y-m-d")."'"; //"date'".date("Y-m-d")."'";    //fills the current date and time in a format that works with our database
         $new_date = date("Y-m-d");
 
-        $event_date_sql = "SELECT * FROM submissions WHERE event_date = '".$new_date."' )";
+        $event_date_sql = "SELECT * FROM submissions WHERE event_date = $new_date )";
 
         $event_result = $db->query($event_date_sql) or die ("Error: ".$event_date_sql."<br>".$db->error);
 

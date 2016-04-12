af7bb8bbe5391d2e1635ff418db3fcb1c532fbdb
kannandineshan <d.kannan@rgu.ac.uk>
2016-04-12 07:26:33 +0100

    12/04/2016 Dinesh updates

--- Dinesh/volunteerhome.php
+++ Dinesh/volunteerhome.php
@@ -199,10 +199,9 @@ function get_volunteer_name($email){
 
             <!-- SURVEY QUESTIONS SUMMARY AND SUBMISSION -->
             <section class="form" id="cont7">
-                <p id="summary"><h1>Survey Questions Summary:</h1>
+                <h1>Survey Questions Summary:</h1>
+                <p id="summary"></p>
                 <input type="submit" id="submit" name="submit" value="SUBMIT SURVEY">
-                </p>
-
             </section>
 
         </form>

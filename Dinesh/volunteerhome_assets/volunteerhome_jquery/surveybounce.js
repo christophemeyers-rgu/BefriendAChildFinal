d1e2d136455a751a4aef69107983bfd681070f2e
kannandineshan <d.kannan@rgu.ac.uk>
2016-04-18 15:44:29 +0100

    Dinesh - 18/04/16 updates on volunteerhome

--- Dinesh/volunteerhome_assets/volunteerhome_jquery/surveybounce.js
+++ Dinesh/volunteerhome_assets/volunteerhome_jquery/surveybounce.js
@@ -8,6 +8,8 @@ $( "#start" ).click(function() {
     $( "#welcomepage" ).toggle( "fade", 1000 );
     $( "#surveybar" ).toggle( "fade", 1000 );
     $( "#cont1" ).toggle( "explode", 1000 );
+    }else {
+        $( "#surveyform" ).find(':submit').click()
     }
 });
 

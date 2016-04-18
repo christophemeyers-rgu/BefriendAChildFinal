418936a125390a5473174003c792e209467fb7a7
kannandineshan <d.kannan@rgu.ac.uk>
2016-04-18 19:15:59 +0100

    Dinesh - 18/04/16 updates on volunteerhome

--- Dinesh/volunteerhome_assets/volunteerhome_jquery/surveybounce.js
+++ Dinesh/volunteerhome_assets/volunteerhome_jquery/surveybounce.js
@@ -1,9 +1,26 @@
 
-
+(function($) {
+    $.fn.safari = function() {
+        return this.each(function() {
+            if (!attributeSupported("required") || ($.browser.safari)) {
+                //If required attribute is not supported or browser is Safari (Safari thinks that it has this attribute, but it does not work), then check all fields that has required attribute
+                $("#formTemplate [required]").each(function(index) {
+                    if (!$(this).val()) {
+                        //If at least one required value is empty, then ask to fill all required fields.
+                        alert("Please fill all required fields.");
+                        return false;
+                    }
+                });
+            }
+            return false; //This is a test form and I'm not going to submit it
+        });
+    };
+}(jQuery));
 //============================================================ Next buttons
 
 
 $( "#start" ).click(function() {
+    $('#eventdate').safari()
     if($('#eventdate')[0].checkValidity()){
         $( "#welcomepage" ).toggle( "fade", 1000 );
         $( "#surveybar" ).toggle( "fade", 1000 );

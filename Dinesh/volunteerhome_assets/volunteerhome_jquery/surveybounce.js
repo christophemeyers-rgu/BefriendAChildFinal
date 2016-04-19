333711824dae5850002cf6b5803cfaa6c0bba233
kannandineshan <d.kannan@rgu.ac.uk>
2016-04-19 20:56:13 +0100

    Dinesh - 18/04/16 updates on volunteerhome

--- Dinesh/volunteerhome_assets/volunteerhome_jquery/surveybounce.js
+++ Dinesh/volunteerhome_assets/volunteerhome_jquery/surveybounce.js
@@ -7,7 +7,7 @@ $( "#start" ).click(function() {
 
     //Checks if browser supports required attribute using Modernizr's feature detection JavaScript code
     if (Modernizr.formvalidation) {
-        //check if field is filled using html5 form validation
+        //Checks if field is filled using html5 form validation
         if ($('#eventdate')[0].checkValidity()) {
             $("#welcomepage").toggle("fade", 1000);
             $("#surveybar").toggle("fade", 1000);
@@ -16,7 +16,7 @@ $( "#start" ).click(function() {
             $("#surveyform").find(':submit').click()
         }
     }else {
-        //If required attribute is not supported then manually check if field is filled
+        //If required attribute is not supported by browser then manually check if field is filled
         if (!$('#eventdate').val()){
             alert("Please Enter the Date of Event.");
         }else{
@@ -31,6 +31,7 @@ $( "#next2" ).click(function() {
 
     //Checks if browser supports required attribute using Modernizr's feature detection JavaScript code
     if (Modernizr.formvalidation) {
+        //Checks if field is filled using html5 form validation
         if ($('#question1')[0].checkValidity()) {
             $("#cont1").toggle("explode", 1000);
             $("#cont2").toggle("size", 1000);
@@ -41,9 +42,9 @@ $( "#next2" ).click(function() {
             $("#surveyform").find(':submit').click()
         }
     }else {
-        //If required attribute is not supported then manually check if field is filled
+        //If required attribute is not supported by browser then manually check if field is filled
         if (!$('#question1').val()){
-            alert("Please fill required field.");
+            alert("Please fill in required field.");
         }else{
             $("#cont1").toggle("explode", 1000);
             $("#cont2").toggle("size", 1000);
@@ -55,64 +56,146 @@ $( "#next2" ).click(function() {
 });
 
 $( "#next3" ).click(function() {
-    if($('#question2')[0].checkValidity()) {
-    $( "#cont2" ).toggle( "size", 1000 );
-    $( "#cont3" ).toggle( "fold", 1000 );
-    $( "#progressbar" ).progressbar({
-            value: 34
-        });
+
+    //Checks if browser supports required attribute using Modernizr's feature detection JavaScript code
+    if (Modernizr.formvalidation) {
+        //Checks if field is filled using html5 form validation
+        if ($('#question2')[0].checkValidity()) {
+            $("#cont2").toggle("size", 1000);
+            $("#cont3").toggle("fold", 1000);
+            $("#progressbar").progressbar({
+                value: 34
+            });
+        } else {
+            $("#surveyform").find(':submit').click()
+        }
     }else {
-        $( "#surveyform" ).find(':submit').click()
+        //If required attribute is not supported by browser then manually check if field is filled
+        if (!$('#question2').val()){
+            alert("Please fill in required field.");
+        }else{
+            $("#cont2").toggle("size", 1000);
+            $("#cont3").toggle("fold", 1000);
+            $("#progressbar").progressbar({
+                value: 34
+            });
+        }
     }
 });
 
 $( "#next4" ).click(function() {
-    if($('#question3')[0].checkValidity()) {
-    $( "#cont3" ).toggle( "fold", 1000 );
-    $( "#cont4" ).toggle( "puff", 1000 );
-    $( "#progressbar" ).progressbar({
-            value: 51
-        });
+
+    //Checks if browser supports required attribute using Modernizr's feature detection JavaScript code
+    if (Modernizr.formvalidation) {
+        //Checks if field is filled using html5 form validation
+        if ($('#question3')[0].checkValidity()) {
+            $("#cont3").toggle("fold", 1000);
+            $("#cont4").toggle("puff", 1000);
+            $("#progressbar").progressbar({
+                value: 51
+            });
+        } else {
+            $("#surveyform").find(':submit').click()
+        }
     }else {
-        $( "#surveyform" ).find(':submit').click()
+        //If required attribute is not supported by browser then manually check if field is filled
+        if (!$('#question3').val()){
+            alert("Please fill in required field.");
+        }else{
+            $("#cont3").toggle("fold", 1000);
+            $("#cont4").toggle("puff", 1000);
+            $("#progressbar").progressbar({
+                value: 51
+            });
+        }
     }
 });
 
 $( "#next5" ).click(function() {
-    if($('#question4')[0].checkValidity()) {
-    $( "#cont4" ).toggle( "puff", 1000 );
-    $( "#cont5" ).toggle( "clip", 1000 );
-    $( "#progressbar" ).progressbar({
-            value: 68
-        });
+
+    //Checks if browser supports required attribute using Modernizr's feature detection JavaScript code
+    if (Modernizr.formvalidation) {
+        //Checks if field is filled using html5 form validation
+        if ($('#question4')[0].checkValidity()) {
+            $("#cont4").toggle("puff", 1000);
+            $("#cont5").toggle("clip", 1000);
+            $("#progressbar").progressbar({
+                value: 68
+            });
+        } else {
+            $("#surveyform").find(':submit').click()
+        }
     }else {
-        $( "#surveyform" ).find(':submit').click()
+        //If required attribute is not supported by browser then manually check if field is filled
+        if (!$('#question4').val()){
+            alert("Please fill in required field.");
+        }else{
+            $("#cont4").toggle("puff", 1000);
+            $("#cont5").toggle("clip", 1000);
+            $("#progressbar").progressbar({
+                value: 68
+            });
+        }
     }
 });
 
 $( "#next6" ).click(function() {
-    if($('#question5')[0].checkValidity()) {
-    $( "#cont5" ).toggle( "clip", 1000 );
-    $( "#cont6" ).toggle( "puff", 1000 );
-    $( "#progressbar" ).progressbar({
-            value: 85
-        });
+
+    //Checks if browser supports required attribute using Modernizr's feature detection JavaScript code
+    if (Modernizr.formvalidation) {
+        //Checks if field is filled using html5 form validation
+        if ($('#question5')[0].checkValidity()) {
+            $("#cont5").toggle("clip", 1000);
+            $("#cont6").toggle("puff", 1000);
+            $("#progressbar").progressbar({
+                value: 85
+            });
+        } else {
+            $("#surveyform").find(':submit').click()
+        }
     }else {
-        $( "#surveyform" ).find(':submit').click()
+        //If required attribute is not supported by browser then manually check if field is filled
+        if (!$('#question5').val()){
+            alert("Please fill in required field.");
+        }else{
+            $("#cont5").toggle("clip", 1000);
+            $("#cont6").toggle("puff", 1000);
+            $("#progressbar").progressbar({
+                value: 85
+            });
+        }
     }
 });
 
 $( "#nextsurveysummary" ).click(function() {
-    if($('#question6')[0].checkValidity()) {
-    $( "#cont6" ).toggle( "fade", 1000 );
-    $( "#cont7" ).toggle( "fade", 1000 );
-    $( "div" ).show().prependTo( "p" ); // Sends all the questions summary to the summary page
 
-    $( "#progressbar" ).progressbar({
-            value: 100
-        });
+    //Checks if browser supports required attribute using Modernizr's feature detection JavaScript code
+    if (Modernizr.formvalidation) {
+        //Checks if field is filled using html5 form validation
+        if ($('#question6')[0].checkValidity()) {
+            $("#cont6").toggle("fade", 1000);
+            $("#cont7").toggle("fade", 1000);
+            $("div").show().prependTo("p"); // Sends all the questions summary to the summary page
+
+            $("#progressbar").progressbar({
+                value: 100
+            });
+        } else {
+            $("#surveyform").find(':submit').click()
+        }
     }else {
-        $( "#surveyform" ).find(':submit').click()
+        //If required attribute is not supported by browser then manually check if field is filled
+        if (!$('#question6').val()){
+            alert("Please fill in required field.");
+        }else{
+            $("#cont6").toggle("fade", 1000);
+            $("#cont7").toggle("fade", 1000);
+            $("div").show().prependTo("p"); // Sends all the questions summary to the summary page
+
+            $("#progressbar").progressbar({
+                value: 100
+            });
+        }
     }
 });
 

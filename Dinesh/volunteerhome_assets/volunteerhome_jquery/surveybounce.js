baaed9cc2134e7638270a5d5b20a25ce4976542b
kannandineshan <d.kannan@rgu.ac.uk>
2016-04-20 20:12:51 +0100

    20/04/2016 Dinesh updates

--- Dinesh/volunteerhome_assets/volunteerhome_jquery/surveybounce.js
+++ Dinesh/volunteerhome_assets/volunteerhome_jquery/surveybounce.js
@@ -207,10 +207,16 @@ $( "#submit" ).click(function() {
 
         }else {
         //If required attribute is not supported by browser then manually check if field is filled
-        if (!$('#summary').val()){
-            alert("Please fill in required field.");
-        }
+        $("#surveyform").each(function(index) {
+            if (!$(this).val()) {
+                //If at least one required value is empty, then ask to fill all required fields.
+                alert("Please fill all required fields.");
+                return false;
+            }
+        });
     }
+    return false; //This is a test form and I'm not going to submit it
+
 });
 
 

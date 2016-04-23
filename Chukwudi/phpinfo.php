ab03d3c23819fd114da25e20a1eec9bdad53da66
chudiezeks <chukwudiezekwesili@windowslive.com>
2016-04-23 18:55:14 +0100

    update

--- Chukwudi/phpinfo.php
+++ Chukwudi/phpinfo.php
@@ -1,24 +1,6 @@
 <?php
 
-    /*
-
-     * You can use a phpinfo() page to view the current PHP information for your server.
-
-     * This file outputs a large amount of information, such as:
-
-         * Information about PHP compilation options and extensions
-         * PHP version
-         * Server information and environment (if compiled as a module)
-         * PHP environment
-         * OS version information, paths, master and local values of configuration options
-         * H TTP headers
-         * PHP license
-
-     * Because every system is set up differently, phpinfo() is commonly used to check PHP configuration settings and for available predefined variables on your particular system.
-
-     * phpinfo() is also a valuable debugging tool, as it contains all EGPCS (Environment, GET, POST, Cookie, and Server) data.
-
-     */
+    //WHAT IS THE PURPOSE OF THIS?
 
     phpinfo();
 ?>

ed84a656a93a86efe7807695af824aa1749c6c26
1510215 <d.kannan@rgu.ac.uk>
2016-04-25 19:10:17 +0100

    25/04/16 Dinesh Last Updates

--- Dinesh/jsadminpage/jquery/enabledisablegender.js
+++ Dinesh/jsadminpage/jquery/enabledisablegender.js
@@ -13,3 +13,22 @@ $('#no').click(function () {
         $('.disabledelements').attr('disabled', true); //disable input
     }
 });
+
+
+
+$( "#submit" ).click(function() {
+
+    //Checks if browser supports required attribute using Modernizr's feature detection JavaScript code
+    if (Modernizr.formvalidation) {
+        //Checks if field is filled using html5 form validation
+            $("form").find(':submit').click()
+
+    }else {
+        //If required attribute is not supported by browser then manually check if field is filled
+        if (!$('form').val()){
+            alert("Please Enter the Date of Event.");
+        }else{
+            $("form").find(':submit').click()
+        }
+    }
+}

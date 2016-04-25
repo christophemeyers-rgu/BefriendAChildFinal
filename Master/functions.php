89a5d9d9d2adddac2fbea480db9f1f021cafbb95
Christophe <christophe.meyers@outlook.com>
2016-04-25 18:38:42 +0100

    Chris 25/04 - Making delete-user display all volunteer attributes.

--- Master/functions.php
+++ Master/functions.php
@@ -199,10 +199,13 @@ function updateUser() {
         unlink($imageurl_old);
     }
     
-    $sql = "update volunteers set vol_email='$login_name',vol_password='$password',vol_firstname='$firstName',vol_surname='$surName',child_matched='$childMatched' where vol_email='$login_name_prev'";
+    $sql = "update volunteers set vol_email='$login_name',vol_password='$password',vol_firstname='$firstName',vol_surname='$surName',vol_child_matched=".$childMatched." where vol_email='$login_name_prev'";
 
     $mysqli = new mysqli(host, user, password, database);
-    $mysqli->query($sql);
+
+
+    $mysqli->query($sql) or die("Error: ".$sql."<br>".$mysqli->error);
+
     $mysqli->close();
     
     }

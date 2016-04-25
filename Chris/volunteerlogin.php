582b31df71fac6080ca8b63a3451f0c2ecaf9c19
Christophe <christophe.meyers@outlook.com>
2016-04-25 19:01:48 +0100

    Chris 25/04 - Making volunteerlogin.php sql injection safe.

--- Chris/volunteerlogin.php
+++ Chris/volunteerlogin.php
@@ -55,15 +55,22 @@
 			die('Connection failed:'.connect_error);
 		}
 		else{
+
+			//Preventing sql injection by preparing the statment
+
 			//select all values from database using the entered values as filter
-			$query="SELECT `vol_email`, `vol_password`
-						FROM `volunteers`
-						WHERE `vol_email` = '$email' AND `vol_password` = '$password' LIMIT 1";
+			$query="SELECT vol_email, vol_password
+						FROM volunteers
+						WHERE vol_email = ? AND BINARY vol_password =BINARY ?";
+
+			$stmt = $db->prepare($query);
+			$stmt->bind_param("ss",$_POST['u'],$_POST['p']);
+			$stmt->execute() or die("Error: ".$query."<br>".$db->error);
 			$output=$db->query($query) or die("Selection Query Failed !!!");
 
 
 			//if the sql query returns a value
-			if(mysqli_num_rows($output)){
+			if(mysqli_stmt_fetch($stmt)){
 				return TRUE; //indicate that a value was returned, and user exists in database
 			}
 			else{

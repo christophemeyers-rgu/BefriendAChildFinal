799b82d9f07b70239af231608f84fc714452c528
kannandineshan <d.kannan@rgu.ac.uk>
2016-04-25 17:33:49 +0100

    25/04/16 Dinesh Updates

--- Dinesh/volunteerhome_assets/volunteerhome_htmlscripts/surveysummaryandsubmission.php
+++ Dinesh/volunteerhome_assets/volunteerhome_htmlscripts/surveysummaryandsubmission.php
@@ -1,7 +1,7 @@
 <h1>Survey Questions Summary:</h1>
 <p id="summary">
     <h5></h5>
-    <div id="surveyquestions"></div>
+    <div id="surveyquestionssummary"></div>
 </p>
 <!-- Submit Survey Button -->
 <input type="submit" id="submit" name="submit" value="SUBMIT SURVEY">

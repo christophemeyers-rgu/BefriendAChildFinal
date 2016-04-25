e2a6f4e01c9c1e0ba74f6a8f92676a2e8886fa84
Christophe <christophe.meyers@outlook.com>
2016-04-25 18:01:54 +0100

    Chris 25/04 - Making edit user display whether volunteer is matched with a child.

--- Chris/edit-user.php
+++ Chris/edit-user.php
@@ -231,13 +231,13 @@
 
                                                             <!--This if-clause should maybe be javascript... I think-->
                                                             <?php
-                                                                if ($row['vol_child_matched']=true){
+                                                                if ($row['vol_child_matched']==true){
                                                                     ?>
                                                                     <input type="radio" name="child_matched" value=true checked >Yes
                                                                     <input type="radio" name="child_matched" value=false>No
                                                                     <?php
                                                                 }
-                                                                else if ($row['vol_child_matched']=false){
+                                                                else if ($row['vol_child_matched']==false){
                                                                     ?>
 
                                                                     <input type="radio" name="child_matched" value=true>Yes

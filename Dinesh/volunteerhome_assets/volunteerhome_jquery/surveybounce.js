0b725d496c01d6bf4005efe7d4ecc4227e070f3c
kannandineshan <d.kannan@rgu.ac.uk>
2016-04-25 15:51:25 +0100

    25/04/16 Dinesh Updates

--- Dinesh/volunteerhome_assets/volunteerhome_jquery/surveybounce.js
+++ Dinesh/volunteerhome_assets/volunteerhome_jquery/surveybounce.js
@@ -175,7 +175,7 @@ $( "#nextsurveysummary" ).click(function() {
         if ($('#question6')[0].checkValidity()) {
             $("#cont6").toggle("slide", 0);
             $("#surveysummaryandsubmission").toggle("fade", 2000);
-            $("div").show().prependTo("p"); // Sends all the questions summary to the summary page
+            $(".s").show().prependTo("p"); // Sends all the questions summary to the summary page
 
             $("#progressbar").progressbar({
                 value: 100

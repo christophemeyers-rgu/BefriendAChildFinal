98533c1e3006ab0cee2e6d495bdb7d8d13920f23
Christophe <christophe.meyers@outlook.com>
2016-04-25 18:43:14 +0100

    Chris 25/04 - Making createlogin.php sql injection safe.

--- Chris/createlogin.php
+++ Chris/createlogin.php
@@ -67,14 +67,12 @@
 
 				if($child_matched==true){
 					$child_gender=$_POST['child_gender'];
-					$day=$_POST['day'];
-					$month=$_POST['month'];
-					$year=$_POST['year'];
-					$dob="date'".$year."-".$month."-".$day."'";
+					$child_date_of_birth = $_POST['date_of_birth'];
+					$dob="date'".$child_date_of_birth."'";
 				}
 				else{
 					$child_gender="other";
-					$dob="date'0000-00-00'";
+					$dob="date'1991-03-12'";
 				}
 
 				$insert="INSERT INTO volunteers (vol_email, vol_password, vol_firstname,vol_surname,vol_child_matched,vol_child_gender,vol_child_dob) VALUES('".$email."','".$password."','".$firstname."','".$surname."',".$child_matched.",'".$child_gender."',".$dob.")";

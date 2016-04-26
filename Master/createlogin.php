0b143c508479b713b5bc40fcdf5904bea1cd0a8e
Christophe <christophe.meyers@outlook.com>
2016-04-26 04:58:52 +0100

    Chris 26/04 - Fixing update

--- Master/createlogin.php
+++ Master/createlogin.php
@@ -69,13 +69,16 @@
 					$child_gender=$_POST['child_gender'];
 					$child_date_of_birth = $_POST['date_of_birth'];
 					$dob="date'".$child_date_of_birth."'";
+
+					$insert="INSERT INTO volunteers (vol_email, vol_password, vol_firstname,vol_surname,vol_child_matched,vol_child_gender,vol_child_dob)
+						 VALUES('".$email."','".$password."','".$firstname."','".$surname."',".$child_matched.",'".$child_gender."',".$dob.")";
 				}
 				else{
-					$child_gender="other";
-					$dob="date'1991-03-12'";
+					$insert="INSERT INTO volunteers (vol_email, vol_password, vol_firstname,vol_surname,vol_child_matched,vol_child_gender,vol_child_dob)
+						 VALUES('".$email."','".$password."','".$firstname."','".$surname."',".$child_matched.",NULL,NULL)";
 				}
 
-				$insert="INSERT INTO volunteers (vol_email, vol_password, vol_firstname,vol_surname,vol_child_matched,vol_child_gender,vol_child_dob) VALUES('".$email."','".$password."','".$firstname."','".$surname."',".$child_matched.",'".$child_gender."',".$dob.")";
+
 
 				$outcome=$db->query($insert) or die("Error: ".$insert."<br>".$db->error);
 

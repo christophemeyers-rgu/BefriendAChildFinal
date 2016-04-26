a3c871b315fdcb75a537856edb91fa5f88a8893e
kannandineshan <d.kannan@rgu.ac.uk>
2016-04-26 04:09:42 +0100

    updates

--- Dinesh/edit-user.php
+++ Dinesh/edit-user.php
@@ -181,21 +181,21 @@
                                                             <?php
                                                             if ($row['vol_child_gender']=="male"){
                                                                 ?>
-                                                                <input type="radio" name="child_gender" value="male" class="disabledelements" id="gender" required disabled checked >Male
-                                                                <input type="radio" name="child_gender" value="female" class="disabledelements" id="gender" required disabled>Female
-                                                                <input type="radio" name="child_gender" value="other" class="disabledelements" id="gender" required disabled>Other
+                                                                <input type="radio" name="child_gender" value="male" class="disabledelements" id="gender" required checked >Male
+                                                                <input type="radio" name="child_gender" value="female" class="disabledelements" id="gender" required>Female
+                                                                <input type="radio" name="child_gender" value="other" class="disabledelements" id="gender" required>Other
                                                                 <?php
                                                             }else if ($row['vol_child_gender']=="female"){
                                                             ?>
-                                                                <input type="radio" name="child_gender" value="male" class="disabledelements" id="gender" required disabled>Male
-                                                                <input type="radio" name="child_gender" value="female" class="disabledelements" id="gender" required disabled checked>Female
-                                                                <input type="radio" name="child_gender" value="other" class="disabledelements" id="gender" required disabled>Other
+                                                                <input type="radio" name="child_gender" value="male" class="disabledelements" id="gender" required>Male
+                                                                <input type="radio" name="child_gender" value="female" class="disabledelements" id="gender" required checked>Female
+                                                                <input type="radio" name="child_gender" value="other" class="disabledelements" id="gender" required>Other
                                                             <?php
                                                             }else if ($row['vol_child_gender']=="other"){
                                                             ?>
-                                                                <input type="radio" name="child_gender" value="male" class="disabledelements" id="gender" required disabled>Male
-                                                                <input type="radio" name="child_gender" value="female" class="disabledelements" id="gender" required disabled>Female
-                                                                <input type="radio" name="child_gender" value="other" class="disabledelements" id="gender" required disabled checked>Other
+                                                                <input type="radio" name="child_gender" value="male" class="disabledelements" id="gender" required>Male
+                                                                <input type="radio" name="child_gender" value="female" class="disabledelements" id="gender" required>Female
+                                                                <input type="radio" name="child_gender" value="other" class="disabledelements" id="gender" required checked>Other
                                                             <?php
                                                             }
                                                             ?>

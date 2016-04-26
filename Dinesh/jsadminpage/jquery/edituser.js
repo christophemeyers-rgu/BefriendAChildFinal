3107cd961e237bf9bc5fbfb3d46306bb9bcb9dc4
kannandineshan <d.kannan@rgu.ac.uk>
2016-04-26 04:29:11 +0100

    updates

--- Dinesh/jsadminpage/jquery/edituser.js
+++ Dinesh/jsadminpage/jquery/edituser.js
@@ -1,14 +1,12 @@
 if ($('#no').is(':checked')) {
 
-    $('.disabledelements').attr('disabled', true); //disable input
     $("#childinfo").hide();
+
 }else {
         if ($('#yes').is(':checked')) {
 
             $("#childinfo").show();
 
-        }else if($('#no').is(':checked')) {
-
-            $("#childinfo").hide();
         }
+
 }

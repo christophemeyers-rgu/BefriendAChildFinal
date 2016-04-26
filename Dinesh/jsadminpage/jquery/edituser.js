417d8f8cc038a79db800955a4fc1cbdefa0b0e62
kannandineshan <d.kannan@rgu.ac.uk>
2016-04-26 03:26:27 +0100

    updates

--- Dinesh/jsadminpage/jquery/edituser.js
+++ Dinesh/jsadminpage/jquery/edituser.js
@@ -1,6 +1,6 @@
 $('#yes').is(':checked')(function () {
     //check if radio button is checked
-    if ($(this).is(':checked')) {
+    if ($('#yes').is(':checked')) {
 
         $('.disabledelements').removeAttr('disabled'); //enable input
         $("#childinfo").show();
@@ -9,7 +9,7 @@ $('#yes').is(':checked')(function () {
 
 $('#no').click(function () {
     //check if checkbox is checked
-    if ($(this).is(':checked')) {
+    if ($('#no').is(':checked')) {
 
         $('.disabledelements').attr('disabled', true); //disable input
         $("#childinfo").hide();
